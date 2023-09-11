ba2d3c02879cbdab9327d72df577a43a8159c193
CanRanBan <canranban@gmx.de>
2023-09-12 01:29:01 +0200

    Set source directory variable independent of solution file directory

--- source/Playnite/Extensions/Markup/ThemeFile.cs
+++ source/Playnite/Extensions/Markup/ThemeFile.cs
@@ -84,7 +84,7 @@ namespace Playnite.Extensions.Markup
             lastUserThemeFound = false;
             var defaultTheme = "Default";
             var projectName = mode == ApplicationMode.Fullscreen ? "Playnite.FullscreenApp" : "Playnite.DesktopApp";
-            var slnPath = Path.Combine(Environment.GetEnvironmentVariable("PLAYNITE_SLN", EnvironmentVariableTarget.User), projectName);
+            var slnPath = Path.Combine(Environment.GetEnvironmentVariable("Playnite_Source_Dir", EnvironmentVariableTarget.User), projectName);
             var themePath = Path.Combine(slnPath, "Themes", ThemeManager.GetThemeRootDir(mode), defaultTheme);
             return new ThemeManifest()
             {
