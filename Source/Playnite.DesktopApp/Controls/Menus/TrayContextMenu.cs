2e33889af8246e36ec749a3c8c2253c71441ed7c
CanRanBan <canranban@gmx.de>
2024-03-30 07:16:47 +0100

    Remove mode switch from tray menu

--- Source/Playnite.DesktopApp/Controls/Menus/TrayContextMenu.cs
+++ Source/Playnite.DesktopApp/Controls/Menus/TrayContextMenu.cs
@@ -101,7 +101,6 @@ namespace Playnite.DesktopApp.Controls
 
             Items.Add(new Separator());
             AddMenuChild(Items, "LOCOpenPlaynite", mainModel.ShowWindowCommand);
-            AddMenuChild(Items, "LOCMenuOpenFullscreen", mainModel.OpenFullScreenCommand);
 
             var openClientItem = AddMenuChild(Items, "LOCMenuClients", null);
             foreach (var tool in mainModel.ThirdPartyTools)
